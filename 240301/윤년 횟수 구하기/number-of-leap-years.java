29983a5e0cc252bc2f7766b437d8bcfb3c9e6b77
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-03-01 13:58:48 +0900

    Title: 윤년 횟수 구하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int leapYear = 0;
+        int commonYear = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 100 == 0 && i % 400 != 0) {
+                commonYear++;
+            } else if (i % 4 == 0) {
+                leapYear++;
+            } else{
+                commonYear++;
+            }
+        }
+        System.out.print(leapYear);
+    }
+}
