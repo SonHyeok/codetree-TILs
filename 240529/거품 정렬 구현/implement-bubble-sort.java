05c4b6e2b03f163bfa88da13e7090664ee76c83a
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-05-29 22:03:01 +0900

    Title: 거품 정렬 구현, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import static java.lang.System.out;public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int tmp = 0;
+        int[] intList = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            intList[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < intList.length - 1; i++) {
+            for (int j = 0; j < intList.length - 1; j++) {
+                if (intList[j] > intList[j + 1]) {
+                    tmp = intList[j];
+                    intList[j] = intList[j + 1];
+                    intList[j + 1] = tmp;
+                }
+            }
+        }
+
+//        String collect = Arrays.stream(intList).
+//                mapToObj(String::valueOf).
+//                collect(Collectors.joining(" "));
+
+//        out.println(collect);
+
+        for (int i : intList) {
+            out.print(i + " ");
+        }
+    }
+}
