f9f2d82ff1d81fa70c38c44c34d538fafa1d61c5
Son Hyeok <87377014+SonHyeok@users.noreply.github.com>
2024-06-07 23:00:47 +0900

    Title: 삽입 정렬 구현, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int[] list = new int[count];
+
+        for(int i = 0; i < count; i++){
+            list[i] = sc.nextInt();
+        }
+
+        for(int i = 1; i < list.length; i++){
+            int j = i - 1;
+            int key = list[i];
+
+            while( j >= 0 && list[j] > key){
+                list[j+1] = list[j];
+                j--;
+                list[j+1] = key;
+            }
+        }
+
+        for(int i = 0; i < list.length; i++){
+            System.out.print(list[i] + " ");
+        }
+    }
+}
